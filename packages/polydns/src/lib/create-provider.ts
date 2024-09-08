7dd2fb9b6d5c8f7d86d6b81917bffdf85be93088
Vojtěch Perník <develop@pervoj.cz>
2024-09-08 10:55:46 +0200

    feat: update how provider is created

--- packages/polydns/src/lib/create-provider.ts
+++ packages/polydns/src/lib/create-provider.ts
@@ -4,10 +4,10 @@ import {
   RecordsAdditions,
   RecordsUnionSchemaExtended,
 } from "./schemas/extend-records-schema.js";
-import { RecordsUnionSchemaWithName } from "./schemas/records-schema-with-name.js";
+import { RecordGeneral } from "./schemas/record-schema.js";
 import { PromiseOr } from "./utils/promise-or.js";
 
-type CallerMethodResult<TData = undefined> =
+type ProviderCallerMethodResult<TData = undefined> =
   | (TData extends undefined | null | never
       ? {
           success: true;
@@ -21,37 +21,52 @@ type CallerMethodResult<TData = undefined> =
       error: string;
     };
 
-type CallerConfig<
-  TRecordsAdditions extends RecordsAdditions,
+export type ProviderCaller<
+  TRecordsAdditions extends RecordsAdditions = {},
+  TRecordIdAdditionSchema extends z.AnyZodObject = z.ZodObject<{}>,
   TRecordsUnionSchema extends
     RecordsUnionSchemaBaseSkeleton = RecordsUnionSchemaExtended<TRecordsAdditions>,
+  TRecord = RecordGeneral<TRecordsUnionSchema>,
 > = {
   listRecords: () => PromiseOr<
-    CallerMethodResult<
-      z.infer<RecordsUnionSchemaWithName<TRecordsUnionSchema>>[]
-    >
+    ProviderCallerMethodResult<(TRecord & z.infer<TRecordIdAdditionSchema>)[]>
   >;
-  createRecord: (
-    record: z.infer<RecordsUnionSchemaWithName<TRecordsUnionSchema>>
-  ) => PromiseOr<CallerMethodResult>;
+  createRecord: (record: TRecord) => PromiseOr<ProviderCallerMethodResult>;
   deleteRecord: (
-    record: z.infer<RecordsUnionSchemaWithName<TRecordsUnionSchema>>
-  ) => PromiseOr<CallerMethodResult>;
+    record: TRecord & z.infer<TRecordIdAdditionSchema>
+  ) => PromiseOr<ProviderCallerMethodResult>;
 };
 
 export function createProvider<
   TRecordsAdditions extends RecordsAdditions = {},
   TCallerConfigSchema extends z.AnyZodObject | undefined = undefined,
+  TRecordIdAdditionSchema extends z.AnyZodObject = z.ZodObject<{}>,
 >(provider: {
   schemas?: {
+    recordIdAddition?: TRecordIdAdditionSchema;
     recordsAdditions?: TRecordsAdditions;
     callerConfig?: TCallerConfigSchema;
   };
   createCaller: TCallerConfigSchema extends undefined
-    ? () => CallerConfig<TRecordsAdditions>
+    ? () => ProviderCaller<TRecordsAdditions, TRecordIdAdditionSchema>
     : (
         config: z.infer<NonNullable<TCallerConfigSchema>>
-      ) => CallerConfig<TRecordsAdditions>;
+      ) => ProviderCaller<TRecordsAdditions, TRecordIdAdditionSchema>;
 }) {
   return provider;
 }
+
+export type Provider<
+  TRecordsAdditions extends RecordsAdditions = {},
+  TCallerConfigSchema extends z.AnyZodObject | undefined = undefined,
+  TRecordIdAdditionSchema extends z.AnyZodObject = z.ZodObject<{}>,
+> = ReturnType<
+  typeof createProvider<
+    TRecordsAdditions,
+    TCallerConfigSchema,
+    TRecordIdAdditionSchema
+  >
+>;
+
+export type RecordForCaller<C extends ProviderCaller<any, any, any>> =
+  C extends ProviderCaller<infer _, infer _, infer _, infer R> ? R : never;
